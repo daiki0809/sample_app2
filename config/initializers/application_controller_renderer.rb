1012d0939818c376b50967894ce5543cd032ae66
田中　大樹 <tanakadaiki@tanakataijunoMacBook-Pro.local>
2019-09-07 17:15:46 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fa2b2c9b95278ad0fd9a845ac27b6d90cac4c1bb81bd76dce59a69a6a8a7b0bd25837cd83a76fe522440f9c2e032e2db0ded07812308732bf2636844f414d075'
